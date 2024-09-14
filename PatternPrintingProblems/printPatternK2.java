4a150d21cdef5c5ec181910fcdfcd2af94a8d67a
Hritik Kushwah <147616034+hritikkushwah12@users.noreply.github.com>
2024-09-14 23:53:01 +0530

    Updated question in printPatternK2.java

--- PatternPrintingProblems/printPatternK2.java
+++ PatternPrintingProblems/printPatternK2.java
@@ -1,3 +1,16 @@
+QUESTION:Print below pattern according to the input n given by the user for further customization in pattern by the user:
+given here n=8
+A A A A A A A A 
+B B B B B B B B 
+C C C C C C C C 
+D D D D D D D D 
+E E E E E E E E 
+F F F F F F F F 
+G G G G G G G G 
+H H H H H H H H     
+SOLUTION:
+
+
 import java.util.Scanner;
 public class printPatternK2 {
     public static void main(String []args){
