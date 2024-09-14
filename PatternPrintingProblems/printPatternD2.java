e31df19f31cb032265bcb1c383e8e13634f1a440
Hritik Kushwah <147616034+hritikkushwah12@users.noreply.github.com>
2024-09-14 23:34:57 +0530

    Updated question in printPatternD2.java

--- PatternPrintingProblems/printPatternD2.java
+++ PatternPrintingProblems/printPatternD2.java
@@ -1,3 +1,14 @@
+Question:Print below pattern only:
+1 2 3 4 5 6 7 
+2 3 4 5 6 7 1 
+3 4 5 6 7 1 2 
+4 5 6 7 1 2 3 
+5 6 7 1 2 3 4 
+6 7 1 2 3 4 5 
+7 1 2 3 4 5 6 
+SOLUTION:
+
+
 public class printPatternD2{
     public static void main(String []args){
         for(int i=1;i<=7;i++){
