8667cdf8639a34707d7efe6c0f70d2259d4699e7
Hritik Kushwah <147616034+hritikkushwah12@users.noreply.github.com>
2024-09-15 00:00:19 +0530

    Updated question in printPatternmM2.java

--- PatternPrintingProblems/printPatternmM2.java
+++ PatternPrintingProblems/printPatternmM2.java
@@ -1,3 +1,14 @@
+QUESTION:Print below pattern according to the input n given by the user for further customization in pattern by the user:
+given here n=6
+ABCDEF
+GHIJKL
+MNOPQR
+STUVWX
+YZ[\]^
+_`abcd
+SOLUTION:
+
+
 import java.util.Scanner;
 public class printPatternmM2{
     public static void main(String []args){
