6e3d0076d34bad81c1a45938269c4db03c455fa9
Hritik Kushwah <147616034+hritikkushwah12@users.noreply.github.com>
2024-09-14 23:30:50 +0530

    Updated question in printPatternC2.java

--- PatternPrintingProblems/printPatternC2.java
+++ PatternPrintingProblems/printPatternC2.java
@@ -1,3 +1,11 @@
+QUESTION:Print below pattern only:
+* * * * 
+* * * 
+* * 
+* 
+SOLUTION:
+
+
 public class printPatternC2{
     public static void main(String []args){
         for(int i=1;i<=4;i++){
