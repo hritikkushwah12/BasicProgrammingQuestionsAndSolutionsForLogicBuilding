dd1b6dd74fac78c5b4c530c50da23aa6f6af083e
Hritik Kushwah <147616034+hritikkushwah12@users.noreply.github.com>
2024-09-14 23:49:46 +0530

    Updated question in printPatternJ2.java

--- PatternPrintingProblems/printPatternJ2.java
+++ PatternPrintingProblems/printPatternJ2.java
@@ -1,3 +1,19 @@
+QUESTION:Print below pattern according to the input n given by the user for further customization in pattern by the user:
+given here n=11
+1 
+2 1 
+3 2 1 
+4 3 2 1 
+5 4 3 2 1 
+6 5 4 3 2 1 
+7 6 5 4 3 2 1 
+8 7 6 5 4 3 2 1 
+9 8 7 6 5 4 3 2 1 
+10 9 8 7 6 5 4 3 2 1 
+11 10 9 8 7 6 5 4 3 2 1 
+SOLUTION:
+
+
 import java.util.Scanner;
 public class printPatternJ2{
     public static void main(String []args){
