b06295ebce29b552f7e1cfeb228b244c1e46a202
Hritik Kushwah <147616034+hritikkushwah12@users.noreply.github.com>
2024-09-14 23:45:39 +0530

    Updated question in printPatternH2.java

--- PatternPrintingProblems/printPatternH2.java
+++ PatternPrintingProblems/printPatternH2.java
@@ -1,3 +1,15 @@
+QUESTION:Print below pattern according to the input n given by the user for further customization in pattern by the user:
+given here n=7
+1 
+2 3 
+3 4 5 
+4 5 6 7 
+5 6 7 8 9 
+6 7 8 9 10 11 
+7 8 9 10 11 12 13 
+SOLUTION:
+
+
 import java.util.Scanner;
 public class printPatternH2{
     public static void main(String []args){
